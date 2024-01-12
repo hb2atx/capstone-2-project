ad020f61d3abfacfbc39e2fb5b47cf5502279432
hb2atx <josephkillelea@gmail.com>
2024-01-12 00:13:29 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,130 @@
+import requests
+from bs4 import BeautifulSoup as bs
+from flask import Flask
+from models import db, connect_db, PlayerInfo, PlayerStats, PlayerSalary, AverageJoeStats, AverageJoeSalary
+from pprint import pprint
+
+app = Flask(__name__)
+app.app_context().push()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///nba_app_db'
+
+
+connect_db(app)
+
+
+all_info = {}
+
+url = "https://www.basketball-reference.com/contracts/players.html"
+r = requests.get(url)
+
+soup = bs(r.text, "html.parser")
+rows = soup.find_all("td", {'class': 'left', 'data-stat': 'player'})
+player_names = [td.find('a').text for td in rows if td.find('a')]
+
+current_salaries = soup.find_all("td", {'class': 'right', 'data-stat': 'y1'})
+current_salaries = [td.text for td in current_salaries if td.text]
+
+players_with_salaries = dict(zip(player_names, current_salaries))
+
+stats_request = requests.get("https://www.basketball-reference.com/leagues/NBA_2024_per_game.html")
+
+soup_two = bs(stats_request.text, "html.parser")
+stats = soup_two.find_all("tr", {'class': 'full_table'})
+my_set = player_names
+
+
+for row in stats:
+    name = row.find("td", {'data-stat': 'player'}).text
+    if not name or not players_with_salaries.get(name):
+        continue
+    if name in my_set:
+        continue
+    my_set.add(name)
+
+
+    all_info[name] = {
+        'salary': players_with_salaries[name],
+        'stats': {
+            'games_played': row.find("td", {'data-stat': "g"}).text,
+            'points_per_game': row.find("td", {'data-stat': 'pts_per_g'}).text,
+            'assists_per_game': row.find("td", {'data-stat': 'ast_per_g'}).text,
+            'defensive_rebounds_per_game': row.find("td", {'data-stat': 'drb_per_g'}).text,
+            'offensive_rebounds_per_game': row.find("td", {'data-stat': 'orb_per_g'}).text,
+            'total_rebounds_per_game': row.find("td", {'data-stat': 'trb_per_g'}).text,
+            'steals_per_game': row.find("td", {'data-stat': 'stl_per_g'}).text,
+            'turnovers_per_game': row.find("td", {'data-stat': 'tov_per_g'}).text,
+            'blocks_per_game': row.find("td", {'data-stat': 'blk_per_g'}).text,
+            'turnovers_per_game': row.find("td", {'data-stat': 'tov_per_g'}).text,
+            'minutes_per_game': row.find("td", {'data-stat': 'mp_per_g'}).text,
+            'two_point_field_goals_per_game': row.find("td", {'data-stat': 'fg2_per_g'}).text,
+            'two_point_field_goal_attempts_per_game': row.find("td", {'data-stat': 'fg2a_per_g'}).text,
+            'two_point_field_goal_percentage': row.find("td", {'data-stat': 'fg2_pct'}).text,
+            'field_goals_per_game': row.find("td", {'data-stat': 'fg_per_g'}).text,
+            'field_goal_attempts_per_game': row.find("td", {'data-stat': 'fga_per_g'}).text,
+            'field_goal_percentage': row.find("td", {'data-stat': 'fg_pct'}).text,
+            'three_point_percentage': row.find("td", {'data-stat': 'fg3_pct'}).text,
+            'three_point_field_goal_attempts_per_game': row.find("td", {'data-stat': 'fg3a_per_g'}).text,
+            'three_point_field_goals_per_game': row.find("td", {'data-stat': 'fg3_per_g'}).text,
+            'free_throw_attempts_per_game': row.find("td", {'data-stat': 'fta_per_g'}).text,
+            'free_throws_per_game': row.find("td", {'data-stat': 'ft_per_g'}).text,
+            'free_throw_percentage': row.find("td", {'data-stat': 'ft_pct'}).text,
+            'players_name': row.find("td", {'data-stat': 'player'}).text,
+            'position': row.find("td", {'data-stat': 'pos'}).text,
+            'age' : row.find("td", {'data-stat': 'age'}).text,
+            'team_id' : row.find("td", {'data-stat': 'team_id'}).text,
+    
+            }
+            
+            }
+    for key, value in all_info.items():
+   
+        players_stats = PlayerStats(
+            games_played = value['stats']['games_played'] or 0,
+            points_per_game = value['stats']['points_per_game'] or 0,
+            assists_per_game = value['stats']['assists_per_game'] or 0,
+            turnovers_per_game = value['stats']['turnovers_per_game'] or 0,
+            defensive_rebounds_per_game = value['stats']['defensive_rebounds_per_game'] or 0,
+            offensive_rebounds_per_game = value['stats']['offensive_rebounds_per_game'] or 0,
+            total_rebounds_per_game = value['stats']['total_rebounds_per_game'] or 0,
+            steals_per_game = value['stats']['steals_per_game'] or 0,
+            blocks_per_game = value['stats']['blocks_per_game'] or 0,
+            minutes_per_game = value['stats']['minutes_per_game'] or 0,
+            two_point_field_goals_per_game = value['stats']['two_point_field_goals_per_game'] or 0,
+            two_point_field_goal_percentage = value['stats']['two_point_field_goal_percentage'] or 0,
+            two_point_field_goal_attempts_per_game = value['stats']['two_point_field_goal_attempts_per_game'] or 0,
+            field_goal_percentage = value['stats']['field_goal_percentage'] or 0,
+            field_goal_attempts_per_game = value['stats']['field_goal_attempts_per_game'] or 0,
+            field_goals_per_game = value['stats']['field_goals_per_game'] or 0,
+            three_point_field_goal_attempts_per_game = value['stats']['three_point_field_goal_attempts_per_game'] or 0,
+            three_point_field_goals_per_game = value['stats']['three_point_field_goals_per_game'] or 0,
+            three_point_percentage = value['stats']['three_point_percentage'] or 0,
+            free_throw_attempts_per_game = value['stats']['free_throw_attempts_per_game'] or 0,
+            free_throws_per_game = value['stats']['free_throws_per_game'] or 0,
+            free_throw_percentage = value['stats']['free_throw_percentage'] or 0
+    )
+        db.session.add(players_stats)
+        db.session.commit()
+
+        players_info = PlayerInfo(
+            players_name = value['stats'].get('name', 'no name'),
+            age = value['stats'].get('age', 'no age'),
+            position = value['stats'].get('positions', 'no position'),
+            team = value['stats'].get('team_id', 'no team')
+        )
+        db.session.add(players_info)
+        db.session.commit()
+
+       
+
+
+
+  
+  
+
+   
+    
+
+
+
+
---
+++ models.py
@@ -0,0 +1,107 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    with app.app_context():
+        db.app = app
+        db.init_app(app)
+        db.drop_all()
+        db.create_all()
+
+
+class PlayerInfo(db.Model):
+
+    __tablename__ = "players_info"
+
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    players_name = db.Column(db.String,nullable=False,unique=True)
+    age = db.Column(db.Integer,nullable=False)
+    position = db.Column(db.String,nullable=False)
+    team = db.Column(db.String,nullable=False)
+    
+class PlayerStats(db.Model):
+
+    __tablename__ = "players_stats"
+
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    games_played = db.Column(db.Integer,nullable=False)
+    minutes_per_game = db.Column(db.Float,nullable=False)
+    field_goal_attempts_per_game = db.Column(db.Float, nullable=False)
+    field_goal_percentage = db.Column(db.Float,nullable=False)
+    field_goals_per_game = db.Column(db.Float, nullable=False)
+    two_point_field_goals_per_game = db.Column(db.Float, nullable=False)
+    two_point_field_goal_attempts_per_game = db.Column(db.Float, nullable=False)
+    two_point_field_goal_percentage = db.Column(db.Float, nullable=False)
+    three_point_field_goal_attempts_per_game = db.Column(db.Float, nullable=False)
+    three_point_field_goals_per_game = db.Column(db.Float, nullable=False)
+    three_point_percentage = db.Column(db.Float,nullable=False)
+    free_throw_attempts_per_game = db.Column(db.Float, nullable=False)
+    free_throws_per_game = db.Column(db.Float, nullable=False)
+    free_throw_percentage = db.Column(db.Float,nullable=False)
+    total_rebounds_per_game = db.Column(db.Float,nullable=False)
+    offensive_rebounds_per_game = db.Column(db.Float, nullable=False)
+    defensive_rebounds_per_game = db.Column(db.Float, nullable=False)
+    assists_per_game = db.Column(db.Float,nullable=False)
+    steals_per_game = db.Column(db.Float,nullable=False)
+    blocks_per_game = db.Column(db.Float,nullable=False)
+    points_per_game = db.Column(db.Float,nullable=False)
+    turnovers_per_game = db.Column(db.Float, nullable=False)
+
+class PlayerSalary(db.Model):
+
+    __tablename__ = "player_salaries"
+
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    first_name = db.Column(db.Integer,nullable=False)
+    last_name = db.Column(db.Integer,nullable=False)
+    salary = db.Column(db.Integer,nullable=False)
+    
+class AverageJoeStats(db.Model):
+
+    __tablename__ = "avg_joe_stats"
+
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    games_played = db.Column(db.Integer,nullable=False)
+    minutes_per_game = db.Column(db.Float,nullable=False)
+    field_goal_attempts_per_game = db.Column(db.Float, nullable=False)
+    field_goal_percentage = db.Column(db.Float,nullable=False)
+    field_goals_per_game = db.Column(db.Float, nullable=False)
+    two_point_field_goals_per_game = db.Column(db.Float, nullable=False)
+    two_point_field_goal_attempts_per_game = db.Column(db.Float, nullable=False)
+    two_point_field_goal_percentage = db.Column(db.Float, nullable=False)
+    three_point_field_goal_attempts_per_game = db.Column(db.Float, nullable=False)
+    three_point_field_goals_per_game = db.Column(db.Float, nullable=False)
+    three_point_percentage = db.Column(db.Float,nullable=False)
+    free_throw_attempts_per_game = db.Column(db.Float, nullable=False)
+    free_throws_per_game = db.Column(db.Float, nullable=False)
+    free_throw_percentage = db.Column(db.Float,nullable=False)
+    total_rebounds_per_game = db.Column(db.Float,nullable=False)
+    offensive_rebounds_per_game = db.Column(db.Float, nullable=False)
+    defensive_rebounds_per_game = db.Column(db.Float, nullable=False)
+    assists_per_game = db.Column(db.Float,nullable=False)
+    steals_per_game = db.Column(db.Float,nullable=False)
+    blocks_per_game = db.Column(db.Float,nullable=False)
+    points_per_game = db.Column(db.Float,nullable=False)
+    turnovers_per_game = db.Column(db.Float, nullable=False)
+
+    
+class AverageJoeSalary(db.Model):
+
+    __tablename__ = "avg_joe_salaries"
+
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    first_name = db.Column(db.Integer,nullable=False)
+    last_name = db.Column(db.Integer,nullable=False)
+    salary = db.Column(db.Float,nullable=False)
+
+
+
+    
+    
+    
+    
+    
+
+
